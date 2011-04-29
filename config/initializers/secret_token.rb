ce81845346b340df32c4f6d09ff8e8b08ea203b8
DanielKao <kao.yucheng@gmail.com>
2011-04-29 03:42:53 -0400

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreateRepositoryGit::Application.config.secret_token = 'c2bdbf5caf11c75441f313f61e04ac8cccae511643309c3e8df859e29b07c7da88fd8f038cae69ba3095af4588313d55d87d77444d4e0cd85070783116eea9c4'
